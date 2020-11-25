4ce786fbccc5613f755e900d25b320d0f3ecdda6
qqm <852371942@qq.com>
2020-11-25 15:09:46 +0800

    覃清敏第一次

---
+++ components/Footer.vue
@@ -0,0 +1,31 @@
+<template>
+    <el-row>
+    <el-col :span="24">
+        <div class="grid-content bg-purple-dark grid-content-p">
+           <p> Copyright ©2017 粤ICP 备 15019482 号 广东碧桂园职业学院版权所有</p>
+        </div>
+        </el-col>
+    </el-row>
+</template>
+
+<style>
+  .el-row {
+    margin-bottom: 20px;
+  }
+  
+  .bg-purple-dark {
+    text-align: center;
+    background: #7699ad;
+  }
+  
+  .grid-content {
+    min-height: 36px;
+    
+    }
+    .grid-content-p{
+        height: 50px;
+        color: white;
+        padding: 14.5px;
+    }
+  
+</style>
---
+++ components/Header.vue
@@ -0,0 +1,45 @@
+<template>
+    <el-row>
+    <el-col :span="4">
+        <div class="grid-content bg-purple1">
+            <img src="..\images\权限-角色管理_01.png" alt="">
+        </div>
+    </el-col>
+    <el-col :span="16">
+        <div class="grid-content bg-purple">
+            <img src="..\images\权限-角色管理_06.png" alt="">
+        </div>
+    </el-col>
+    <el-col :span="4">
+        <div class="grid-content bg-purple">
+            <el-button type="text" class="el-button-1"><img src="..\images\权限-角色管理_04.png" alt=""> 个人中心</el-button>
+            <el-button type="text" class="el-button-1"><img src="..\images\权限-角色管理_02.png" alt=""> 安全退出</el-button>
+            </div></el-col>
+    </el-row>
+</template>
+
+<style>
+  .el-row {
+    min-width: 1350px;
+    margin-bottom: 20px;
+  }
+  
+  .bg-purple {
+    padding: 20.5px;
+    background: #007aa3;
+  }
+  .bg-purple1{
+    min-width: 260px;
+    padding: 10px;
+    background: #005284;
+  }
+ 
+  .grid-content {
+    min-height: 36px;
+  }
+  .el-button-1{
+      padding: 8px;
+      /* text-align: center; */
+      color: white;
+  }
+</style>
