d9862e405acf74e8408037989d8e6aafc3ae421b
qqm <852371942@qq.com>
2020-11-26 09:26:28 +0800

    Revert "覃清敏第二次"
    This reverts commit 585ba79fdf766bf5d66aa9764f6597073d580931.

--- components/Header.vue
+++ components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-    <el-row class="el-header-row">
+    <el-row>
     <el-col :span="4">
         <div class="grid-content bg-purple1">
             <img src="..\images\权限-角色管理_01.png" alt="">
@@ -11,7 +11,7 @@
         </div>
     </el-col>
     <el-col :span="4">
-        <div class="grid-content bg-purple bg-purple2">
+        <div class="grid-content bg-purple">
             <el-button type="text" class="el-button-1"><img src="..\images\权限-角色管理_04.png" alt=""> 个人中心</el-button>
             <el-button type="text" class="el-button-1"><img src="..\images\权限-角色管理_02.png" alt=""> 安全退出</el-button>
             </div></el-col>
@@ -19,7 +19,7 @@
 </template>
 
 <style>
-  .el-header-row {
+  .el-row {
     min-width: 1350px;
     margin-bottom: 20px;
   }
@@ -28,13 +28,8 @@
     padding: 20.5px;
     background: #007aa3;
   }
-  .bg-purple2{
-    padding-left: 0px;
-    min-width: 225px;
-    background: #007aa3;
-  }
   .bg-purple1{
-    min-width: 225px;
+    min-width: 260px;
     padding: 10px;
     background: #005284;
   }
--- pages/test.vue
+++ pages/test.vue
@@ -1,15 +1,5 @@
 <template>
     <div>
-        <Header />
-        <el-row>
-            <el-col :span="4">
-                <Navbar />
-            </el-col>
-            <el-col :span="20">
-                <Table />
-                <Pagination />
-            </el-col>
-        </el-row>
-        <Footer />
+        <Navbar />
     </div>
 </template>
