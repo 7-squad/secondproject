865ebd99fbcbbb6692298e55569d922cc852870b
qqm <852371942@qq.com>
2020-11-26 09:31:26 +0800

    Revert "Revert "覃清敏第二次""
    This reverts commit d9862e405acf74e8408037989d8e6aafc3ae421b.

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,31 +1,33 @@
 <template>
-    <el-row>
+  <el-row class="el-footer-row">
     <el-col :span="24">
-        <div class="grid-content bg-purple-dark grid-content-p">
-           <p> Copyright ©2017 粤ICP 备 15019482 号 广东碧桂园职业学院版权所有</p>
-        </div>
-        </el-col>
-    </el-row>
+      <div class="grid-content bg-purple-dark grid-content-p">
+        <p>Copyright ©2017 粤ICP 备 15019482 号 广东碧桂园职业学院版权所有</p>
+      </div>
+    </el-col>
+  </el-row>
 </template>
 
 <style>
-  .el-row {
-    margin-bottom: 20px;
-  }
-  
-  .bg-purple-dark {
-    text-align: center;
-    background: #7699ad;
-  }
-  
-  .grid-content {
-    min-height: 36px;
-    
-    }
-    .grid-content-p{
-        height: 50px;
-        color: white;
-        padding: 14.5px;
-    }
-  
+.el-row {
+  min-width: 1350px;
+}
+.el-footer-row {
+  margin-top: 20px;
+  min-width: 1350px;
+}
+
+.bg-purple-dark {
+  text-align: center;
+  background: #7699ad;
+}
+
+.grid-content {
+  min-height: 36px;
+}
+.grid-content-p {
+  height: 50px;
+  color: white;
+  padding: 14.5px;
+}
 </style>
